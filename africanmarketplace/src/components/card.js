113207869124a2562147defd2e9f023f539ad6aa
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-07-27 13:28:28 -0400

    Last marge failed with cards due to no refectoring

---
+++ africanmarketplace/src/components/card.js
@@ -0,0 +1,53 @@
+import "../src/Card.scss";
+import React from "react";
+
+function Card() {
+	return (
+		<div className="wrapper">
+			<Card
+				img={"https://source.unsplash.com/1600x900/?eggs"}
+				title="Eggs"
+				description=""
+			/>
+			<Card
+				img={"https://source.unsplash.com/1600x900/?milk,gallon"}
+				title="Milk"
+				description=""
+			/>
+			<Card
+				img={"https://source.unsplash.com/1600x900/?fish,ocean"}
+				title="Fish"
+				description=""
+			/>
+			<Card
+				img={"https://source.unsplash.com/1600x900/?honey,hive"}
+				title="Honey"
+				description=""
+			/>
+			<Card
+				img={"https://source.unsplash.com/1600x900/?meat,filet"}
+				title="Meat"
+				description=""
+			/>
+			<Card
+				img={"https://source.unsplash.com/1600x900/?poultry"}
+				title="Poultry"
+				description=""
+			/>
+		</div>
+	);
+}
+
+function Card(props) {
+	return (
+<>    
+    <div className="card">
+    <div className="cardBody">
+        <img src={props.img} class="cardImage" alt="" />
+        <h2 className="cardTitle">{props.title}</h2>
+        <p className="cardDescription">{props.description}</p>
+    </div>
+    <button className="cardButton">View Products</button>
+    </div>
+</>)
+    
