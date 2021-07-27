db89dc3a4568ba488f1ca68dfaddd9ab899b01bd
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-07-26 21:46:07 -0400

    installed scss extension and styled

--- africanmarketplace/src/App.js
+++ africanmarketplace/src/App.js
@@ -1,9 +1,29 @@
-import "./App.css";
+import "./App.scss";
 import React from "react";
 import ReactDOM from "react-dom";
+import eggs from "../src/assets/eggs.jpg";
 
 function App() {
-  return <div className="wrapper"></div>;
+  return (
+    <div className="wrapper">
+      <Card img={eggs} title="Eggs" description="Local Eggs" />
+      <Card img={eggs} title="Eggs" description="Local Eggs" />
+      <Card img={eggs} title="Eggs" description="Local Eggs" />
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="cardBody">
+        <img src={props.img} class="cardImage" />
+        <h2 className="cardTitle">{props.title}</h2>
+        <p className="cardDescription">{props.description}</p>
+      </div>
+      <button className="cardButton">Add To Cart</button>
+    </div>
+  );
 }
 
 export default App;
