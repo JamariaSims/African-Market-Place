45dcbf09456fb8c815574e07d0790a830b6666f6
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-07-27 17:54:29 -0400

    Making Nav More Robust

--- africanmarketplace/src/App.js
+++ africanmarketplace/src/App.js
@@ -1,37 +1,47 @@
 import "./App.css";
+import { useState } from "react";
 import firebase from "firebase/app";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./views/HomePage/home";
-import Navbar from "./views/navbar";
 import OwnerView from "./views/OwnerPage/OwnerView";
 
+// Firebase Area //
 const firebaseConfig = {
-  apiKey: "AIzaSyB2wyupUcSOEyvODy4QECjjxqHHZJShXvY",
-  authDomain: "african-market-place.firebaseapp.com",
-  databaseURL: "https://african-market-place-default-rtdb.firebaseio.com",
-  projectId: "african-market-place",
-  storageBucket: "african-market-place.appspot.com",
-  messagingSenderId: "127459408307",
-  appId: "1:127459408307:web:b681ffacf04a56864533c6",
-  measurementId: "G-KED6X0V47R",
+	apiKey: "AIzaSyB2wyupUcSOEyvODy4QECjjxqHHZJShXvY",
+	authDomain: "african-market-place.firebaseapp.com",
+	databaseURL: "https://african-market-place-default-rtdb.firebaseio.com",
+	projectId: "african-market-place",
+	storageBucket: "african-market-place.appspot.com",
+	messagingSenderId: "127459408307",
+	appId: "1:127459408307:web:b681ffacf04a56864533c6",
+	measurementId: "G-KED6X0V47R",
 };
-
 firebase.initializeApp(firebaseConfig);
+//
 
 function App() {
-  return (
-    <Router>
-      <Route path="/">
-        <Navbar />
-      </Route>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/user">
-        <OwnerView />
-      </Route>
-    </Router>
-  );
+	const [Page, setPage] = useState({
+		PageName: "Products",
+		Tabs: [
+			`All Products`,
+			"Animal Products",
+			"Vegetables",
+			"Fruits",
+			"Roots&Tubers",
+			"Others",
+		],
+	});
+	return (
+		<Router>
+			<Route path="/"></Route>
+			<Route exact path="/">
+				<Home PageName={Page.PageName} Tabs={Page.Tabs} />
+			</Route>
+			<Route path="/user">
+				<OwnerView />
+			</Route>
+		</Router>
+	);
 }
 
 export default App;
--- africanmarketplace/src/views/HomePage/home.js
+++ africanmarketplace/src/views/HomePage/home.js
@@ -1,14 +1,30 @@
-import Cards from "../../components/card";
 import React from "react";
+import Navbar from "../../components/navbar";
 
-function Home() {
-  return (
-    <>
-      <div>
-        <Cards />
-      </div>
-    </>
-  );
+function Home(props) {
+	const { PageName, Tabs } = props;
+	console.log(Tabs);
+	return (
+		<>
+			<Navbar PageName={PageName} Tabs={Tabs} />
+			<div className="main-container">
+				<div>
+					<h3>Sauti Marketplace</h3>
+					<p>
+						Our mission is To empower women and families around the world, with
+						equal business opportunity through culture.
+					</p>
+					<div>Leave Us Feedback</div>
+				</div>
+				<img
+					src={"https://source.unsplash.com/1600x900/?africa"}
+					width={"500px"}
+				/>
+			</div>
+			<div className="1-container"></div>
+			<div className="2-container"></div>
+		</>
+	);
 }
 
 export default Home;
