dedc57772ccebdc4d7fea83cf1bd11b7809bcbe8
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-07-26 21:12:57 -0400

    testing

--- africanmarketplace/src/App.js
+++ africanmarketplace/src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="wrapper"></div>;
 }
 
 export default App;
+
+ReactDOM.render(<App />, document.getElementById("root"));
