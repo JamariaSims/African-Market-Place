a57a3cc3450a1fb8c7a6622531d0e1399561075f
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-07-27 17:01:26 -0400

    added card to product page

--- africanmarketplace/src/views/navbar.js
+++ africanmarketplace/src/views/navbar.js
@@ -5,73 +5,73 @@ import { Link } from "react-router-dom";
 import profile from "../assets/profile.png";
 
 function Navbar() {
-	const [action, setAction] = useState("");
-	const [users, setUsers] = useState([]);
-	const [userData, setUserData] = useState({
-		email: "",
-		password: "",
-		userName: "",
-	});
-	return (
-		<>
-			<AuthLogin
-				userData={userData}
-				setUserData={setUserData}
-				action={action}
-				setAction={setAction}
-				users={users}
-				setUsers={setUsers}
-			/>
-			<div className="home">
-				<nav>
-					<div className="searchBar">
-						<h1>Products</h1>
-						<div className="input">
-							<input type="text" />
-							<Link to="/cart">
-								<img width="35%" src={cart} />
-							</Link>
+  const [action, setAction] = useState("");
+  const [users, setUsers] = useState([]);
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+    userName: "",
+  });
+  return (
+    <>
+      <AuthLogin
+        userData={userData}
+        setUserData={setUserData}
+        action={action}
+        setAction={setAction}
+        users={users}
+        setUsers={setUsers}
+      />
+      <div className="home">
+        <nav>
+          <div className="searchBar">
+            <h1>Products</h1>
+            <div className="input">
+              <input type="text" />
+              <Link to="/cart">
+                <img width="35%" src={cart} />
+              </Link>
 
-							{action === "SignedIn" ? (
-								<>
-									<div
-										id="loginBtn"
-										onClick={() => {
-											document.getElementById("form").classList.toggle("hide");
-										}}
-									>
-										{userData.userName}
-									</div>
-									<Link to="/User">
-										<img width="50%" src={profile} />
-									</Link>
-								</>
-							) : (
-								<>
-									<div
-										id="loginBtn"
-										onClick={() => {
-											document.getElementById("form").classList.toggle("hide");
-										}}
-									>
-										{"Login"}
-									</div>
-								</>
-							)}
-						</div>
-					</div>
-					<div className="cards">
-						<div className="tab1">All Products</div>
-						<div className="tab2">Animal Products</div>
-						<div className="tab3">Vegetables</div>
-						<div className="tab4">Fruits</div>
-						<div className="tab5">Roots&Tubers</div>
-						<div className="tab6">Others</div>
-					</div>
-				</nav>
-			</div>
-		</>
-	);
+              {action === "SignedIn" ? (
+                <>
+                  <div
+                    id="loginBtn"
+                    onClick={() => {
+                      document.getElementById("form").classList.toggle("hide");
+                    }}
+                  >
+                    {userData.userName}
+                  </div>
+                  <Link to="/User">
+                    <img width="50%" src={profile} />
+                  </Link>
+                </>
+              ) : (
+                <>
+                  <div
+                    id="loginBtn"
+                    onClick={() => {
+                      document.getElementById("form").classList.toggle("hide");
+                    }}
+                  >
+                    {"Login"}
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+          <div className="cards">
+            <div className="tab1">All Products</div>
+            <div className="tab2">Animal Products</div>
+            <div className="tab3">Vegetables</div>
+            <div className="tab4">Fruits</div>
+            <div className="tab5">Roots&Tubers</div>
+            <div className="tab6">Others</div>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
