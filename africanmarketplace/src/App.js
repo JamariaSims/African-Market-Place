5cb12e6be7148c58276ea4ff73567a4e74f3e625
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-07-28 16:55:52 -0400

    updated files from css to scss

--- africanmarketplace/src/App.js
+++ africanmarketplace/src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import firebase from "firebase/app";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./views/HomePage/home";
@@ -10,39 +10,39 @@ import Layout from "./views/OwnerPage/OwnerView";
 
 // Firebase Area //
 const firebaseConfig = {
-	apiKey: "AIzaSyB2wyupUcSOEyvODy4QECjjxqHHZJShXvY",
-	authDomain: "african-market-place.firebaseapp.com",
-	databaseURL: "https://african-market-place-default-rtdb.firebaseio.com",
-	projectId: "african-market-place",
-	storageBucket: "african-market-place.appspot.com",
-	messagingSenderId: "127459408307",
-	appId: "1:127459408307:web:b681ffacf04a56864533c6",
-	measurementId: "G-KED6X0V47R",
+  apiKey: "AIzaSyB2wyupUcSOEyvODy4QECjjxqHHZJShXvY",
+  authDomain: "african-market-place.firebaseapp.com",
+  databaseURL: "https://african-market-place-default-rtdb.firebaseio.com",
+  projectId: "african-market-place",
+  storageBucket: "african-market-place.appspot.com",
+  messagingSenderId: "127459408307",
+  appId: "1:127459408307:web:b681ffacf04a56864533c6",
+  measurementId: "G-KED6X0V47R",
 };
 firebase.initializeApp(firebaseConfig);
 //
 
 function App() {
-	return (
-		<Router>
-			<Route path="/">
-				<LoginModal />
-			</Route>
-			<Route exact path="/">
-				<Home />
-			</Route>
-			<Route path="/user">
-				<OwnerView />
-			</Route>
-			<Route path="/fish">
-				<FishCards />
-			</Route>
-			<Route path="/nope">
-				<Layout />
-			</Route>
-			<Footer />
-		</Router>
-	);
+  return (
+    <Router>
+      <Route path="/">
+        <LoginModal />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/user">
+        <OwnerView />
+      </Route>
+      <Route path="/fish">
+        <FishCards />
+      </Route>
+      <Route path="/nope">
+        <Layout />
+      </Route>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
