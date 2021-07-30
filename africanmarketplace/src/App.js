84f91e0cfee56535fe2b0cddcab72ffa9a28cad0
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-07-30 02:20:09 -0400

    fixing errors

--- africanmarketplace/src/App.js
+++ africanmarketplace/src/App.js
@@ -1,50 +1,50 @@
 import firebase from "firebase/App";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./Views/HomePage/Home";
-import OwnerView from "./Views/OwnerPage/OwnerView";
+import Home from "./views/HomePage/Home";
+import OwnerView from "./views/OwnerPage/OwnerView";
 import Footer from "./components/Modals/FooterBar";
 import { useState, useEffect } from "react";
-import MarketPage from "./Views/MarketPage/MarketPage";
+import MarketPage from "./views/MarketPage/MarketPage";
 import SearchBar2 from "./components/SearchBar2";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyB2wyupUcSOEyvODy4QECjjxqHHZJShXvY",
-	authDomain: "african-market-place.firebaseapp.com",
-	databaseURL: "https://african-market-place-default-rtdb.firebaseio.com",
-	projectId: "african-market-place",
-	storageBucket: "african-market-place.appspot.com",
-	messagingSenderId: "127459408307",
-	appId: "1:127459408307:web:b681ffacf04a56864533c6",
-	measurementId: "G-KED6X0V47R",
+  apiKey: "AIzaSyB2wyupUcSOEyvODy4QECjjxqHHZJShXvY",
+  authDomain: "african-market-place.firebaseapp.com",
+  databaseURL: "https://african-market-place-default-rtdb.firebaseio.com",
+  projectId: "african-market-place",
+  storageBucket: "african-market-place.appspot.com",
+  messagingSenderId: "127459408307",
+  appId: "1:127459408307:web:b681ffacf04a56864533c6",
+  measurementId: "G-KED6X0V47R",
 };
 firebase.initializeApp(firebaseConfig);
 function App() {
-	const [userData, setUserData] = useState({
-		username: "",
-		password: "",
-		email: "",
-		Login: false,
-		SignUp: false,
-		Forgot: false,
-	});
-	useEffect(() => {}, [userData]);
-	return (
-		<Router>
-			<Route exact path="/">
-				<Home userData={userData} setUserData={setUserData} />
-			</Route>
-			<Route path="/user">
-				<OwnerView />
-			</Route>
-			<Route path="/searchbar2">
-				<SearchBar2 />
-			</Route>
-			<Route path="/MarketPage">
-				<MarketPage userData={userData} setUserData={setUserData} />
-			</Route>
-			<Footer />
-		</Router>
-	);
+  const [userData, setUserData] = useState({
+    username: "",
+    password: "",
+    email: "",
+    Login: false,
+    SignUp: false,
+    Forgot: false,
+  });
+  useEffect(() => {}, [userData]);
+  return (
+    <Router>
+      <Route exact path="/">
+        <Home userData={userData} setUserData={setUserData} />
+      </Route>
+      <Route path="/user">
+        <OwnerView />
+      </Route>
+      <Route path="/searchbar2">
+        <SearchBar2 />
+      </Route>
+      <Route path="/MarketPage">
+        <MarketPage userData={userData} setUserData={setUserData} />
+      </Route>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
--- africanmarketplace/src/components/ImageData.js
+++ africanmarketplace/src/components/ImageData.js
@@ -6,24 +6,24 @@ import twitter from "../Assets/twitter.png";
 import React from "react";
 
 function ImageData(props) {
-	const { imgName } = props;
-	switch (imgName) {
-		case "facebook": {
-			return <img width={"40%"} src={facebook} />;
-		}
-		case "google": {
-			return <img width={"40%"} src={google} />;
-		}
-		case "ig": {
-			return <img width={"45%"} src={ig} />;
-		}
-		case "twitter": {
-			return <img width={"40%"} src={twitter} />;
-		}
-		default: {
-			break;
-		}
-	}
+  const { imgName } = props;
+  switch (imgName) {
+    case "facebook": {
+      return <img width={"40%"} src={facebook} />;
+    }
+    case "google": {
+      return <img width={"40%"} src={google} />;
+    }
+    case "ig": {
+      return <img width={"45%"} src={ig} />;
+    }
+    case "twitter": {
+      return <img width={"40%"} src={twitter} />;
+    }
+    default: {
+      break;
+    }
+  }
 }
 
 export default ImageData;
