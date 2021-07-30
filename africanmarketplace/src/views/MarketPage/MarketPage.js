10800c4ac41e3ae6538a4178d64592b7dedb6353
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-07-30 07:46:25 -0400

    Search fixed now show good data

--- africanmarketplace/src/views/MarketPage/MarketPage.js
+++ africanmarketplace/src/views/MarketPage/MarketPage.js
@@ -3,14 +3,14 @@ import NavigationBar from "../../components/Modals/NavigationBar";
 import Listings from "./Listings";
 
 function MarketPage(props) {
-  const { userData, setUserData } = props;
-  const Tabs = ["Home", "My Store"];
-  return (
-    <>
-      <NavigationBar userData={userData} PageName={"MarketPage"} Tabs={Tabs} />
-      <Listings userData={userData} setUserData={setUserData} />
-    </>
-  );
+	const { userData, setUserData } = props;
+	const Tabs = ["Home", "My Store"];
+	return (
+		<>
+			<NavigationBar userData={userData} PageName={"Market Page"} Tabs={Tabs} />
+			<Listings userData={userData} setUserData={setUserData} />
+		</>
+	);
 }
 
 export default MarketPage;
