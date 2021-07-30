fec2f4084ba47426e7361dec3d307c3295eab400
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-07-30 04:54:27 -0400

    form fix

--- africanmarketplace/src/components/OwnerViewComponents/FormDialog.js
+++ africanmarketplace/src/components/OwnerViewComponents/FormDialog.js
@@ -1,129 +1,102 @@
 import React, { useEffect, useState } from "react";
 import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import DialogActions from "@material-ui/core/DialogActions";
-import PropTypes from "prop-types";
-import Avatar from "@material-ui/core/Avatar";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import ListItemText from "@material-ui/core/ListItemText";
-import PersonIcon from "@material-ui/icons/Person";
-import AddIcon from "@material-ui/icons/Add";
 import Typography from "@material-ui/core/Typography";
-import { DialogContent, DialogContentText } from "@material-ui/core";
-
 import Dialog from "@material-ui/core/Dialog";
-
-import { makeStyles } from "@material-ui/core/styles";
-
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-
 import DialogTitle from "@material-ui/core/DialogTitle";
 
-function SimpleDialog(infoData, setInfoData, open, onClose) {
-  const handleClose = () => {
-    onClose();
-  };
-
-  const handleListItemSubmit = (event) => {
-    event.preventDefault();
-  };
-
-  //const { infoData, setInfoData } = props;
-  const [enteredBName, setEnteredBName] = useState("");
-  const [enteredLocal, setEnteredLocal] = useState("");
-  const [enteredContact, setEnteredContact] = useState("");
-
-  const nameChangeHandler = (event) => {
-    setEnteredBName(event.target.value);
-  };
-  const localChangeHandler = (event) => {
-    setEnteredLocal(event.target.value);
-  };
-  const contactChangeHandler = (event) => {
-    setEnteredContact(event.target.value);
-  };
-
-  const informationData = {
-    name: enteredBName,
-    location: enteredLocal,
-    contact: enteredContact,
-  };
-
-  setInfoData({ ...infoData, informationData });
-
-  return (
-    <Dialog
-      onClose={handleClose}
-      aria-labelledby="simple-dialog-title"
-      open={open}
-    >
-      <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
-      <List>
-        <ListItem>
-          <label>Buisness Name </label>
-          <input
-            type="text"
-            value={enteredBName}
-            onChange={nameChangeHandler}
-          ></input>
-        </ListItem>
-        <ListItem>
-          <label>Location </label>
-          <input
-            type="text"
-            value={enteredLocal}
-            onChange={localChangeHandler}
-          ></input>
-        </ListItem>
-        <ListItem>
-          <label>Contact </label>
-          <input
-            type="number"
-            value={enteredContact}
-            onChange={contactChangeHandler}
-          ></input>
-        </ListItem>
-        <ListItem>
-          <button
-            type="submit"
-            onSubmit={() => {
-              handleListItemSubmit();
-            }}
-          >
-            Save Changes
-          </button>
-        </ListItem>
-      </List>
-    </Dialog>
-  );
+export default function SimpleDialogDemo(props) {
+	const { infoData, setInfoData } = props;
+	const [open, setOpen] = React.useState(false);
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+	const handleClose = (value) => {
+		setOpen(false);
+	};
+	// SimpleDialog.propTypes = {
+	//   onClose: PropTypes.func.isRequired,
+	//   open: PropTypes.bool.isRequired,
+	// };
+	return (
+		<div>
+			<Typography variant="subtitle1"></Typography>
+			<br />
+			<Button variant="outlined" color="primary" onClick={handleClickOpen}>
+				Edit
+			</Button>
+			{SimpleDialog(handleClose, setInfoData, infoData, open, setOpen)}
+		</div>
+	);
 }
 
-export default function SimpleDialogDemo(props) {
-  const { infoData, setInfoData } = props;
-  const [open, setOpen] = React.useState(false);
+function SimpleDialog(onClose, setInfoData, infoData, open, setOpen) {
+	const handleClose = () => {};
+	const handleListItemSubmit = (event) => {
+		event.preventDefault();
+	};
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+	//const { infoData, setInfoData } = props;
+	const [enteredBName, setEnteredBName] = useState("");
+	const [enteredLocal, setEnteredLocal] = useState("");
+	const [enteredContact, setEnteredContact] = useState("");
 
-  SimpleDialog.propTypes = {
-    onClose: PropTypes.func.isRequired,
-    open: PropTypes.bool.isRequired,
-  };
+	const nameChangeHandler = (event) => {
+		setEnteredBName(event.target.value);
+	};
+	const localChangeHandler = (event) => {
+		setEnteredLocal(event.target.value);
+	};
+	const contactChangeHandler = (event) => {
+		setEnteredContact(event.target.value);
+	};
 
-  const handleClose = (value) => {
-    setOpen(false);
-  };
+	const informationData = [enteredBName, enteredLocal, enteredContact];
 
-  return (
-    <div>
-      <Typography variant="subtitle1"></Typography>
-      <br />
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Edit
-      </Button>
-      {SimpleDialog(infoData, setInfoData, open, onClose)}
-    </div>
-  );
+	return (
+		<Dialog
+			onClose={handleClose}
+			aria-labelledby="simple-dialog-title"
+			open={open}
+		>
+			<DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
+			<List>
+				<ListItem>
+					<label>Buisness Name </label>
+					<input
+						type="text"
+						value={enteredBName}
+						onChange={nameChangeHandler}
+					></input>
+				</ListItem>
+				<ListItem>
+					<label>Location </label>
+					<input
+						type="text"
+						value={enteredLocal}
+						onChange={localChangeHandler}
+					></input>
+				</ListItem>
+				<ListItem>
+					<label>Contact </label>
+					<input
+						type="number"
+						value={enteredContact}
+						onChange={contactChangeHandler}
+					></input>
+				</ListItem>
+				<ListItem>
+					<button
+						onClick={() => {
+							setInfoData(informationData);
+							setOpen(false);
+						}}
+					>
+						Save Changes
+					</button>
+				</ListItem>
+			</List>
+		</Dialog>
+	);
 }
--- africanmarketplace/src/views/OwnerPage/OwnerView.js
+++ africanmarketplace/src/views/OwnerPage/OwnerView.js
@@ -10,82 +10,70 @@ import SimpleDialogDemo from "../../components/OwnerViewComponents/FormDialog";
 import BuissnesInfo from "../../components/OwnerViewComponents/BuissnesInfo";
 
 function OwnerView() {
-  const ProductList = [
-    {
-      id: "",
-      image: "https://source.unsplash.com/800x900/?eggs",
-      title: "Eggs",
-      price: "3.99",
-      description: "Eggs that come from a chicken",
-    },
+	const ProductList = [
+		{
+			id: "",
+			image: "https://source.unsplash.com/800x900/?eggs",
+			title: "Eggs",
+			price: "3.99",
+			description: "Eggs that come from a chicken",
+		},
 
-    {
-      id: "",
-      image: "https://source.unsplash.com/800x900/?milk",
-      title: "Milk",
-      price: "5.99",
-      description: "Milk that comes from a cow",
-    },
+		{
+			id: "",
+			image: "https://source.unsplash.com/800x900/?milk",
+			title: "Milk",
+			price: "5.99",
+			description: "Milk that comes from a cow",
+		},
 
-    {
-      id: "",
-      image: "https://source.unsplash.com/800x900/?almonds",
-      title: "Almonds",
-      price: "10.99",
-      description: "Nuts that come from a tree",
-    },
-  ];
+		{
+			id: "",
+			image: "https://source.unsplash.com/800x900/?almonds",
+			title: "Almonds",
+			price: "10.99",
+			description: "Nuts that come from a tree",
+		},
+	];
 
-  const [productListData, setProductListData] = useState(ProductList);
+	const [productListData, setProductListData] = useState(ProductList);
 
+	const [infoData, setInfoData] = useState([]);
 
-  const info = [
-    {
-      id:"",
-      name: "",
-      location: "",
-      contact: "",
-    },
-  ];
-  const [infoData, setInfoData] = useState(info);
+	return (
+		<div>
+			<section className="itemContainer">
+				<img src="africanmarketplace\src\assets\profile.png" />
 
-  return (
-    <div>
-      <section className="itemContainer">
-        <img src="africanmarketplace\src\assets\profile.png" />
-        <SimpleDialogDemo 
-        infoData={infoData} 
-        setInfoData={setInfoData} 
-        />
-        {infoData.map((inform) => (
-          <BuissnesInfo
-            name={inform.name}
-            location={inform.location}
-            contact={inform.contact}
-          />
-        ))}
-      </section>
-      <section className="otherWrapper">
-        <p className="titleProducts">Products</p>
-      </section>
-      <section>
-        <ItemForm
-          setProductList={setProductListData}
-          productList={productListData}
-        />
-      </section>
-      <section className="otherWrapper2">
-        {productListData.map((product) => (
-          <AddItem
-            img={product.image}
-            title={product.title}
-            price={product.price}
-            description={product.description}
-          />
-        ))}
-      </section>
-    </div>
-  );
+				<BuissnesInfo
+					name={infoData[0]}
+					location={infoData[1]}
+					contact={infoData[2]}
+				/>
+
+				<SimpleDialogDemo infoData={infoData} setInfoData={setInfoData} />
+			</section>
+			<section className="otherWrapper">
+				<p className="titleProducts">Products</p>
+			</section>
+			<section>
+				<ItemForm
+					setProductList={setProductListData}
+					productList={productListData}
+				/>
+			</section>
+			<section className="otherWrapper2">
+				{productListData.map((product) => (
+					<AddItem
+						img={product.image}
+						title={product.title}
+						price={product.price}
+						description={product.description}
+					/>
+				))}
+			</section>
+		</div>
+	);
 }
 
 export default OwnerView;
