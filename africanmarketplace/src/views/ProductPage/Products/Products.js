e25efa8048fec58f64b5d8c648bd3ccc86f2e19e
spearoxi <spearofdavid@gmail.com>
2021-07-30 19:58:29 -0400

    pulled

--- africanmarketplace/src/views/ProductPage/Products/Products.js
+++ africanmarketplace/src/views/ProductPage/Products/Products.js
@@ -22,122 +22,124 @@ function ProductCards() {
   padding: "2%",
 });
   return (
-    // < className="wrapper">
-    //   <Link>
-    //   <ProductCard
-    //     img={Eggs}
-    //     title="Eggs"
-    //     description=""
-    //   />
-    //   </Link>
+    <div className="wrapper">
+      <Link>
+      <ProductCard
+        img={Eggs}
+        title="Eggs"
+        description=""
+      />
+      </Link>
 
-    //   <ProductCard
-    //     img={Milk}
-    //     title="Milk"
-    //     description=""
-    //   />
-    //   <ProductCard
-    //     img={Fish}
-    //     title="Fish"
-    //     description=""
-    //   />
-    //   <ProductCard
-    //     img={Honey}
-    //     title="Honey"
-    //     description=""
-    //   />
-    //   <ProductCard
-    //     img={Steak}
-    //     title="Meat"
-    //     description=""
-    //   />
-    //   <ProductCard
-    //     img={Poultry}
-    //     title="Poultry"
-    //     description=""
-    //   />
-    //   <ProductCard
-    //     img={Vegetables}
-    //     title="Vegetables"
-    //     description=""
-    //   />
-    //   <ProductCard
-    //     img={Fruits}
-    //     title="Fruits"
-    //     description=""
-    //     onClick
-    //   />
+      <ProductCard
+        img={Milk}
+        title="Milk"
+        description=""
+      />
+      <ProductCard
+        img={Fish}
+        title="Fish"
+        description=""
+      />
+      <ProductCard
+        img={Honey}
+        title="Honey"
+        description=""
+      />
+      <ProductCard
+        img={Steak}
+        title="Meat"
+        description=""
+      />
+      <ProductCard
+        img={Poultry}
+        title="Poultry"
+        description=""
+      />
+      <ProductCard
+        img={Vegetables}
+        title="Vegetables"
+        description=""
+      />
+      <ProductCard
+        img={Fruits}
+        title="Fruits"
+        description=""
+        onClick
+      />
 
-       <ProductCards className="card">
-      <div className="cardBody">
-        <img src={Grains} class="cardImage" alt="" />
-        <h2 className="cardTitle">Cereals</h2>
-        <p className="cardDescription">''</p>
-      <button className="cardButton" href='#'>View Products</button>
-      </div>
+{/* //       <ProductCards className="card">
+//       <div className="cardBody">
+//         <img src={Grains} class="cardImage" alt="" />
+//         <h2 className="cardTitle">Cereals</h2>
+//         <p className="cardDescription">''</p>
+//       <button className="cardButton" href='#'>View Products</button>
+//       </div>
     
 
        
-      <div className="cardBody">
-        <img src={Fruits} class="cardImage" alt="" />
-        <h2 className="cardTitle">Fruits</h2>
-        <p className="cardDescription">''</p>
-      <button className="cardButton" href='#'>View Products</button>
-      </div>
+//       <div className="cardBody">
+//         <img src={Fruits} class="cardImage" alt="" />
+//         <h2 className="cardTitle">Fruits</h2>
+//         <p className="cardDescription">''</p>
+//       <button className="cardButton" href='#'>View Products</button>
+//       </div>
    
 
        
-      <div className="cardBody">
-        <img src={Fish} class="cardImage" alt="" />
-        <h2 className="cardTitle">Fish</h2>
-        <p className="cardDescription">''</p>
-      <button className="cardButton" href='#'>View Products</button>
-      </div>
+//       <div className="cardBody">
+//         <img src={Fish} class="cardImage" alt="" />
+//         <h2 className="cardTitle">Fish</h2>
+//         <p className="cardDescription">''</p>
+//       <button className="cardButton" href='#'>View Products</button>
+//       </div>
    
 
      
-      <div className="cardBody">
-        <img src={Milk} class="cardImage" alt="" />
-        <h2 className="cardTitle">Milk</h2>
-        <p className="cardDescription">''</p>
-      <button className="cardButton" href='#'>View Products</button>
-      </div>
+//       <div className="cardBody">
+//         <img src={Milk} class="cardImage" alt="" />
+//         <h2 className="cardTitle">Milk</h2>
+//         <p className="cardDescription">''</p>
+//       <button className="cardButton" href='#'>View Products</button>
+//       </div>
    
        
-      <div className="cardBody">
-        <img src={Meat} class="cardImage" alt="" />
-        <h2 className="cardTitle">Meat</h2>
-        <p className="cardDescription">''</p>
-      <button className="cardButton" href='#'>View Products</button>
-      </div>
+//       <div className="cardBody">
+//         <img src={Meat} class="cardImage" alt="" />
+//         <h2 className="cardTitle">Meat</h2>
+//         <p className="cardDescription">''</p>
+//       <button className="cardButton" href='#'>View Products</button>
+//       </div>
    
        
-      <div className="cardBody">
-        <img src={Poultry} class="cardImage" alt="" />
-        <h2 className="cardTitle">Poultry</h2>
-        <p className="cardDescription">''</p>
-        <button className="cardButton" href='#'>View Products</button>
-      </div>
+//       <div className="cardBody">
+//         <img src={Poultry} class="cardImage" alt="" />
+//         <h2 className="cardTitle">Poultry</h2>
+//         <p className="cardDescription">''</p>
+//         <button className="cardButton" href='#'>View Products</button>
+//       </div>
       
    
       
-      <div className="cardBody">
-        <img src={Honey} class="cardImage" alt="" />
-        <h2 className="cardTitle">Honey</h2>
-        <p className="cardDescription">''</p>
-      <button className="cardButton" href='#'>View Products</button>
-      </div>
+//       <div className="cardBody">
+//         <img src={Honey} class="cardImage" alt="" />
+//         <h2 className="cardTitle">Honey</h2>
+//         <p className="cardDescription">''</p>
+//       <button className="cardButton" href='#'>View Products</button>
+//       </div>
     
 
 
-<div className="cardBody">
-  <img src={Eggs} class="cardImage" alt="" />
-  <h2 className="cardTitle">Eggs</h2>
-  <p className="cardDescription">''</p>
-<button className="cardButton" href='#'>View Products</button>
-</div>
+// <div className="cardBody">
+//   <img src={Eggs} class="cardImage" alt="" />
+//   <h2 className="cardTitle">Eggs</h2>
+//   <p className="cardDescription">''</p>
+// <button className="cardButton" href='#'>View Products</button>
+// </div>
 
-</ProductCards>
+// </ProductCards> */}
+
+</div> 
 
     
   );
@@ -148,18 +150,18 @@ function ProductCards() {
 
 
 
-// function ProductCard(props) {
+function ProductCard(props) {
  
-//   return (
-//     <div className="card">
-//       <div className="cardBody">
-//         <img src={props.img} class="cardImage" alt="" />
-//         <h2 className="cardTitle">{props.title}</h2>
-//         <p className="cardDescription">{props.description}</p>
-//       </div>
-//       <button className="cardButton" href='#'>View Products</button>
-//     </div>
-//   );
-// }
+  return (
+    <div className="card">
+      <div className="cardBody">
+        <img src={props.img} class="cardImage" alt="" />
+        <h2 className="cardTitle">{props.title}</h2>
+        <p className="cardDescription">{props.description}</p>
+      </div>
+      <button className="cardButton" href='#'>View Products</button>
+    </div>
+  );
+}
 
 export default ProductCards;
