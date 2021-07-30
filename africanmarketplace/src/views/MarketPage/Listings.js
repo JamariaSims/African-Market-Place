3f4fcf26edf9c38cff754b8edc095d81e8eff13f
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-07-30 04:01:41 -0400

    Error Fix

--- africanmarketplace/src/views/MarketPage/Listings.js
+++ africanmarketplace/src/views/MarketPage/Listings.js
@@ -13,88 +13,90 @@ import { AvatarGenerator } from "random-avatar-generator";
 import { Autocomplete } from "@material-ui/lab";
 
 function Listings(props) {
-  const { userData, setUserData } = props;
-  const addToCart = (e) => {
-    // setUserData({ ...userData, ["cart"]: "" });
-    console.log(e.target);
-  };
-  const useStyles = makeStyles({
-    table: {
-      minWidth: 650,
-    },
-  });
-  function createData(
-    Name,
-    Category,
-    VendorAmount,
-    VendorPrice,
-    VendorName,
-    VendorAvatar
-  ) {
-    return {
-      Name,
-      Category,
-      VendorAmount,
-      VendorPrice,
-      VendorName,
-      VendorAvatar,
-    };
-  }
+	const { userData, setUserData } = props;
+	const addToCart = (e) => {
+		// setUserData({ ...userData, ["cart"]: "" });
+		console.log(e.target);
+	};
+	const useStyles = makeStyles({
+		table: {
+			minWidth: 650,
+		},
+	});
+	function createData(
+		Name,
+		Category,
+		VendorAmount,
+		VendorPrice,
+		VendorName,
+		VendorAvatar
+	) {
+		return {
+			Name,
+			Category,
+			VendorAmount,
+			VendorPrice,
+			VendorName,
+			VendorAvatar,
+		};
+	}
 
-  const generator = new AvatarGenerator({});
+	const generator = new AvatarGenerator({});
 
-  const rows = [
-    createData("Maize ", "Cereals", 6, "24$", "Jamaria"),
-    createData("Cool Shirt", "Clothing & Shoes", 9, "37$", "JBars"),
-    createData("Cool Lip Gloss", "Cosmetics", 16, "24$", "DavidS"),
-    createData("Fresh Nile Perch", "Fish Products", 3.7, 67, "BrandonV"),
-    createData("Lime", "Fruits", 16, 49, "JBars"),
-    createData("Beans ", "Legumes", 6, 24, "Jamaria"),
-    createData("Groundnuts", "Pulses", 9, 37, "DavidS"),
-    createData("Timber", "Timber", 16, 24, "Jamaria"),
-    createData("Irish Tubers", "Fish Products", 3, 67, "DavidS"),
-    createData("Onions", "Vegetables", 16, 49, "BrandonV"),
-  ];
-  const classes = useStyles();
-  return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Category</TableCell>
-            <TableCell align="right">Vendor Amount</TableCell>
-            <TableCell align="right">Vendor Price</TableCell>
-            <TableCell align="right">Vendor Name</TableCell>
+	const rows = [
+		createData("Maize ", "Cereals", 6, "24$", "Jamaria"),
+		createData("Cool Shirt", "Clothing & Shoes", 9, "37$", "JBars"),
+		createData("Cool Lip Gloss", "Cosmetics", 16, "24$", "DavidS"),
+		createData("Fresh Nile Perch", "Fish Products", 3.7, 67, "BrandonV"),
+		createData("Lime", "Fruits", 16, 49, "JBars"),
+		createData("Beans ", "Legumes", 6, 24, "Jamaria"),
+		createData("Groundnuts", "Pulses", 9, 37, "DavidS"),
+		createData("Timber", "Timber", 16, 24, "Jamaria"),
+		createData("Irish Tubers", "Fish Products", 3, 67, "DavidS"),
+		createData("Onions", "Vegetables", 16, 49, "BrandonV"),
+	];
+	const classes = useStyles();
+	return (
+		<TableContainer component={Paper}>
+			<Table className={classes.table} aria-label="simple table">
+				<TableHead>
+					<TableRow>
+						<TableCell>Name</TableCell>
+						<TableCell align="right">Category</TableCell>
+						<TableCell align="right">Vendor Amount</TableCell>
+						<TableCell align="right">Vendor Price</TableCell>
+						<TableCell align="right">Vendor Name</TableCell>
 
-            <TableCell align="right"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.Name}
-              </TableCell>
-              <TableCell align="right">{row.Category}</TableCell>
-              <TableCell align="right">{row.VendorAmount}</TableCell>
-              <TableCell align="right">{row.VendorPrice}</TableCell>
-              <TableCell align="right">{row.VendorName}</TableCell>
+						<TableCell align="right"></TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{rows.map((row) => (
+						<TableRow key={row.name}>
+							<TableCell component="th" scope="row">
+								{row.Name}
+							</TableCell>
+							<TableCell align="right">{row.Category}</TableCell>
+							<TableCell align="right">{row.VendorAmount}</TableCell>
+							<TableCell align="right">{row.VendorPrice}</TableCell>
+							<TableCell align="right">{row.VendorName}</TableCell>
+							<TableCell align="center">
+								{" "}
+								<Avatar
+									alt={row.VendorName}
+									src={generator.generateRandomAvatar()}
+								/>
+							</TableCell>
 
-              <Avatar
-                alt={row.VendorName}
-                src={generator.generateRandomAvatar()}
-              />
-
-              <TableCell align="right">
-                <CoolIcons.AddCircleOutlineOutlined />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+							<TableCell align="right">
+								<CoolIcons.AddCircleOutlineOutlined />
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
 }
 
 export default Listings;
