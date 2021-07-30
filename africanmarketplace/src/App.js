ef1a4450c7102564bf6c85645eb127114eb8eb0d
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-07-30 03:43:12 -0400

    Avarter fix

--- africanmarketplace/src/App.js
+++ africanmarketplace/src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import firebase from "firebase";
-=======
 import firebase from "firebase/app";
->>>>>>> f5df7a80a2f9599568063bd6c92074c4545476ee
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./views/HomePage/Home";
 import OwnerView from "./views/OwnerPage/OwnerView";
@@ -12,47 +8,47 @@ import MarketPage from "./views/MarketPage/MarketPage";
 import SearchBar2 from "./components/SearchBar2";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2wyupUcSOEyvODy4QECjjxqHHZJShXvY",
-  authDomain: "african-market-place.firebaseapp.com",
-  databaseURL: "https://african-market-place-default-rtdb.firebaseio.com",
-  projectId: "african-market-place",
-  storageBucket: "african-market-place.appspot.com",
-  messagingSenderId: "127459408307",
-  appId: "1:127459408307:web:b681ffacf04a56864533c6",
-  measurementId: "G-KED6X0V47R",
+	apiKey: "AIzaSyB2wyupUcSOEyvODy4QECjjxqHHZJShXvY",
+	authDomain: "african-market-place.firebaseapp.com",
+	databaseURL: "https://african-market-place-default-rtdb.firebaseio.com",
+	projectId: "african-market-place",
+	storageBucket: "african-market-place.appspot.com",
+	messagingSenderId: "127459408307",
+	appId: "1:127459408307:web:b681ffacf04a56864533c6",
+	measurementId: "G-KED6X0V47R",
 };
 firebase.initializeApp(firebaseConfig);
 function App() {
-  const [userData, setUserData] = useState({
-    username: "",
-    password: "",
-    email: "",
-    Login: false,
-    SignUp: false,
-    Forgot: false,
-  });
-  useEffect(() => {}, [userData]);
-  return (
-    <Router>
-      <Route exact path="/">
-        <Home userData={userData} setUserData={setUserData} />
-      </Route>
-      <Route path="/user">
-        <OwnerView />
-      </Route>
-      <Route path="/searchbar2">
-        <SearchBar2 />
-      </Route>
-      <Route path="/MarketPage">
-        <MarketPage userData={userData} setUserData={setUserData} />
-      </Route>
-      <Route path="/OwnerView">
-        <OwnerView userData={userData} setUserData={setUserData} />
-      </Route>
+	const [userData, setUserData] = useState({
+		username: "",
+		password: "",
+		email: "",
+		Login: false,
+		SignUp: false,
+		Forgot: false,
+	});
+	useEffect(() => {}, [userData]);
+	return (
+		<Router>
+			<Route exact path="/">
+				<Home userData={userData} setUserData={setUserData} />
+			</Route>
+			<Route path="/user">
+				<OwnerView />
+			</Route>
+			<Route path="/searchbar2">
+				<SearchBar2 />
+			</Route>
+			<Route path="/MarketPage">
+				<MarketPage userData={userData} setUserData={setUserData} />
+			</Route>
+			<Route path="/OwnerView">
+				<OwnerView userData={userData} setUserData={setUserData} />
+			</Route>
 
-      <Footer />
-    </Router>
-  );
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
